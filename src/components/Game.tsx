ed8413d08f3832d6e5a147dda18ce5b01f84aca7
Richard Carruthers <richardcarruthers8@gmail.com>
2023-01-20 21:09:04 +0000

    add excalibur game engine

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
+import { Engine } from "excalibur";
 import { Room } from "../types/Room";
 
 export function Game({ room }: { room: Room }) {
+  useEffect(() => {
+    const game = new Engine({
+      width: 800,
+      height: 600,
+    });
+
+    game.start();
+  }, []);
+
   return <div>Game {room.id}</div>;
 }
