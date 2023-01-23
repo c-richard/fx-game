1e1d0032bcb3e043d9709a8df5f2574efffe80ee
Richard Carruthers <richardcarruthers8@gmail.com>
2023-01-23 22:15:13 +0000

    fixup crappy clickable tile

--- src/components/actors/tileActor.ts
+++ src/components/actors/tileActor.ts
@@ -1,14 +1,11 @@
 import {
     Actor,
-    CircleCollider,
     Color,
     Engine,
     Polygon,
     PolygonCollider,
-    vec,
     Vector,
 } from 'excalibur'
-import { mean } from 'rambda'
 import { TileType } from '../../types/types'
 
 class Tile extends Actor {
@@ -26,29 +23,20 @@ class Tile extends Actor {
     }: {
         pos: Vector
         color: Color
-        // radius: number
         polygon: Vector[]
         ownerId: string | null
         type: TileType
     }) {
-        const minX = Math.min(...polygon.map((p) => p.x))
-        const minY = Math.min(...polygon.map((p) => p.y))
-        const avgX = mean(polygon.map((p) => p.x))
-        const avgY = mean(polygon.map((p) => p.y))
-
-        const centeroonied = polygon.map((p) => p.sub(vec(avgX, avgY)))
-
         super({
             ...res,
             pos,
             collider: new PolygonCollider({
-                points: centeroonied,
-                offset: vec(avgX, avgY).sub(pos),
+                points: polygon,
+                offset: pos.negate(),
             }),
         })
 
         this.polygon = polygon
-
         this.ownerId = ownerId
         this.type = type
         this.enableCapturePointer = true
